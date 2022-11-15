dcbfaa59039c14507bfaff4f6563de8b9933eef8
Diogo Carmo <info@diogo.ch>
2022-11-15 16:05:41 +0100

    Rails Task Manager

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -17,23 +17,23 @@ class TasksController < ApplicationController
     redirect_to task_path(@task)
   end
 
-  def edit; end
+  def edit
+    @task = Task.find(params[:id])
+  end
 
   def update
+    @task = Task.find(params[:id])
     @task.update(task_params)
     redirect_to task_path(@task)
   end
 
   def destroy
+    @task = Task.find(params[:id])
     @task.destroy
     redirect_to tasks_path, status: :see_other
   end
 
   private
-
-  def set_task
-    @task = Task.find(params[:id])
-  end
 
   def task_params
     params.require(:task).permit(:title, :details, :completed)
